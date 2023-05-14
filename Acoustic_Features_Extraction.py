055a1d0fbccf15a8ee3afd27e68eaed669b23561
Manan Sheel <51280188+manansheel1991@users.noreply.github.com>
2023-05-14 22:06:46 +0530

    Add files via upload

---
+++ Acoustic_Features_Extraction.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 14:28:50 2023
+
+@author: manan
+"""
+import librosa
+import soundfile as sf
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+import wave
+import webrtcvad
+mysp=__import__("my-voice-analysis")
+from scipy.io import wavfile
+import contextlib
+import io
+import os
+import re
+
+filename = 'common_voice_en (1).wav'
+temp_filename = 'common_voice_en (1)_temp.wav'
+
+def extract_frequency_related_information(filename):
+    
+    ## Function to extract fundamental frequency, mean fundamental frequency, range of frequencies
+    
+    y, samplerate = sf.read('{}'.format(filename)) 
+    y = y[:, 0]
+    chunks = np.array_split(y,int(samplerate/2000))
+    peaks = []
+    
+    for chunk in chunks:
+        wave = chunk
+        # compute the magnitude of the Fourier Transform and its corresponding frequency values
+        freq_magnitudes = np.abs(np.fft.rfft(wave))
+        freq_values = np.fft.rfftfreq(len(wave), 1/samplerate)
+        # find the max. magnitude
+        max_positive_freq_idx = np.argmax(freq_magnitudes)
+        peaks.append(freq_values[max_positive_freq_idx])
+        
+    ff = np.min(peaks)
+    mean_ff = np.mean(peaks)
+    range_ff = np.max(peaks) - np.min(peaks)
+    
+    return ff, mean_ff, range_ff
+
+#ff, mean_ff, range_ff = extract_frequency_related_information(filename)
+
+def extract_amplitude_related_information(filename):
+    
+    ## Function to extract mean amplitude, amplitude variance
+
+    y, samplerate = sf.read('{}'.format(filename))
+    
+    y = y[:, 0]
+    
+    amplitude_mean = np.mean(abs(y))
+    
+    amplitude_variance = np.var(abs(y))
+    
+    return amplitude_mean, amplitude_variance
+
+#a_m, a_v = extract_amplitude_related_information(filename)
+
+#p = 'Sample_Audio_1'
+#
+#c = r'C:\Users\manan\Documents\Confidence Information Speech Task - Interview\Common-Voice-Files-In-WAV-Format'
+#
+#temp_filename = '{filename}_temp.wav'
+
+def convert_audio_file(filename, temp_filename, path):
+    
+    #y, samplerate = sf.read('{}'.format(filename))
+    
+    y, samplerate = librosa.load(r'C:\Users\manan\Documents\Confidence Information Speech Task - Interview\Common-Voice-Files-In-WAV-Format\{}'.format(filename), sr = 44100)
+
+    #y = librosa.core.resample(y, orig_sr = samplerate, target_sr = 44100)
+    
+    sf.write(r'{}/{}'.format(path, temp_filename), y, 44100, 'PCM_16')
+    
+    #librosa.output.write_wav(f"{path}/{temp_filename}", y, samplerate)
+    
+
+def analyze_audio_file(filename, temp_filename, path):
+    
+    ## Function to extract Speech Rate, Pause_Frequency, Balance, Articulation_rate
+    
+    convert_audio_file(filename, temp_filename, path)
+    with io.StringIO() as buf, contextlib.redirect_stdout(buf):
+        mysp.mysptotal(temp_filename[:-4], path)
+        #mysp.mysptotal(filename[:-4], path)
+        captured_output = buf.getvalue()
+        
+        numbers = [float(num) for num in re.findall(r"\d+\.\d+|\d+", captured_output) if num != "0"]
+        numbers = numbers[:14]
+        
+        # remove temp file
+        os.remove(fr"{path}/{temp_filename}")
+
+        audio_params = {
+            "number_of_syllables": numbers[0],
+            "number_of_pauses": numbers[1],
+            "rate_of_speech": numbers[2],
+            "articulation_rate": numbers[3],
+            "speaking_duration": numbers[4],
+            "original_duration": numbers[5],
+            "balance": numbers[6],
+            "pause_frequency": numbers[1]/numbers[5]
+            
+        }
+        
+        return audio_params['rate_of_speech'], audio_params['pause_frequency'], audio_params['balance'], audio_params['articulation_rate']
+      
+#a, b, o, t = analyze_audio_file(filename, temp_filename, path = c)
+
+# convert_audio_file(filename, temp_filename, c)
+
+
---
+++ Confidence_Model_Building.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 15:27:22 2019
+
+@author: manan
+"""
+# Import necessary libraries
+
+import pydub 
+import scipy
+import numpy as np
+from pydub import AudioSegment
+from scipy import signal
+import librosa
+from librosa import core
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+from Acoustic_Features_Extraction import extract_frequency_related_information, extract_amplitude_related_information, analyze_audio_file
+
+import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+import random
+
+import pandas as pd
+
+# Initialize lists for acoustic features, 30 files are taken for the purpose of code demonstration.
+# In reality, there should be thousands of files
+
+ff = [None]*30
+mean_ff = [None]*30
+range_ff = [None]*30
+
+amplitude_mean = [None]*30
+amplitude_variance = [None]*30
+
+speech_rate = [None]*30
+pause_frequency = [None]*30
+balance = [None]*30
+articulation_rate = [None]*30
+
+# For-loops to read the common-voice files, and extract acoustic features
+
+for i in range(0,30):
+    ff[i], mean_ff[i], range_ff[i] = extract_frequency_related_information(r'C:\Users\manan\Documents\Confidence Information Speech Task - Interview\Common-Voice-Files-In-WAV-Format\common_voice_en ({}).wav'.format(i+1))
+    amplitude_mean[i], amplitude_variance[i] = extract_amplitude_related_information(r'C:\Users\manan\Documents\Confidence Information Speech Task - Interview\Common-Voice-Files-In-WAV-Format\common_voice_en ({}).wav'.format(i+1))
+
+for i in range(0, 30):
+    filename = 'common_voice_en ({}).wav'.format(i+1)
+    speech_rate[i], pause_frequency[i], balance[i], articulation_rate[i] = analyze_audio_file(filename, temp_filename = '{}_temp.wav'.format(filename), path = r'C:\Users\manan\Documents\Confidence Information Speech Task - Interview\Common-Voice-Files-In-WAV-Format')
+    
+# Initialize Input Array of Independent Features
+
+x = [[0 for i in range(9)] for j in range(30)] 
+
+for i in range(0, 30):
+        x[i][0] = ff[i]
+        x[i][1] = mean_ff[i]
+        x[i][2] = range_ff[i]
+        x[i][3] = amplitude_mean[i]
+        x[i][4] = amplitude_variance[i]
+        x[i][5] = speech_rate[i]
+        x[i][6] = pause_frequency[i]
+        x[i][7] = balance[i]
+        x[i][8] = articulation_rate[i]
+        
+# Define Target Confidence Scores for training the model, between 0 and 10. 
+# As there is no dataset available with confidence scores, currently, this is put for code demonstration. 
+
+y = [random.randint(0, 10) for _ in range(30)]
+
+## Model in Keras
+
+#define the model 
+model = Sequential()
+
+#add the layers
+model.add(Dense(units=6, activation='relu', input_dim=9))
+model.add(Dense(units=1))
+
+#compile the model
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+#train the model
+model.fit(x,y,epochs=100)
+
+#Save the model to a file
+
+model.save('Confidence_Scores_Model.hdf5', overwrite=True, include_optimizer=True)
+
+# Make a Sample Prediction through the model
+
+model = model.load('Confidence_Scores_Model.hdf5')
+
+xp = [[250, 350, 400, 0.05, 0.006, 5, 0.56, 0.7, 5.0]]
+
+yp = model.predict(xp)
+
